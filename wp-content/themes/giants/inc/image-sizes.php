504d1ee541238dfa3e7b0dd58d54f320b68cf5f1
Cameron Spencer <cameron@lab43.com>
2017-04-14 10:10:49 -0700

    adjust image size

--- wp-content/themes/giants/inc/image-sizes.php
+++ wp-content/themes/giants/inc/image-sizes.php
@@ -3,5 +3,5 @@ add_action( 'after_setup_theme', 'giants_image_sizes' );
 function giants_image_sizes() {
   add_image_size('slideshow', 1375, 1000, false);
   add_image_size('6-column', 640);
-  add_image_size('full-columns', 1100);
+  add_image_size('services-slideshow', 960, 600, true);
 }
